f0ed6b20c432451fc4e06c2c2a2f0d2ee92bee93
Rolf K <abstractstar@gmail.com>
2020-10-24 13:26:09 +0200

    Fix wget script + add readme

--- wget-greg.sh
+++ wget-greg.sh
@@ -8,20 +8,21 @@ PODCASTTITLE="$5"
 
 OUTPUT="${DIRECTORY}/${DATE}-${FILENAMETITLE}.mp3"
 
-RESULT=$(/usr/bin/wget $LINK -O $OUTPUT -a "${DIRECTORY}/${PODCASTTITLE}.log" -c -nv)
+/usr/bin/wget $LINK -O $OUTPUT -a "${DIRECTORY}/${PODCASTTITLE}.log" -c -nv
 
 # Look for errors
-if [[ $RESULT == 0 ]]; then
+if [[ $? == 0 ]]; then
     # No errors
     TITLE="Success!"
     MESSAGE="Downloaded file ${FILENAMETITLE} for podcast ${PODCASTTITLE}!"
     if [[ ! -z $DL_USER && ! -z $DL_GROUP ]]; then
         chown "$DL_USER":"$DL_GROUP" $OUTPUT
+        chown "$DL_USER":"$DL_GROUP" $DIRECTORY
     fi
 else
     # Some error happened!
     TITLE="ERROR!"
-    MESSAGE="Error code ${RESULT} encountered while downloading file ${FILENAMETITLE} for podcast ${PODCASTTITLE}! See man wget for more details."
+    MESSAGE="Error code $? encountered while downloading file ${FILENAMETITLE} for podcast ${PODCASTTITLE}! See man wget for more details."
 fi
 
 # Send notification (if APPRISE_SERVICE is set)
