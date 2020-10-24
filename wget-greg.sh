c2f41d96e76ff3bf83497bc0e9bb35e65b74f07b
Rolf K <abstractstar@gmail.com>
2020-10-24 13:01:40 +0200

    Fix and improve wget script

--- wget-greg.sh
+++ wget-greg.sh
@@ -7,16 +7,18 @@ FILENAMETITLE="$4"
 PODCASTTITLE="$5"
 
 
-RESULT=$(/usr/bin/wget $LINK -O "${DIRECTORY}/${DATE}-${FILENAMETITLE}.mp3" -a "${DIRECTORY}/${PODCASTTITLE}.log -c -nv)
+RESULT=$(/usr/bin/wget $LINK -O "${DIRECTORY}/${DATE}-${FILENAMETITLE}.mp3" -a "${DIRECTORY}/${PODCASTTITLE}.log" -c -nv)
 
 MESSAGE=""
 
-if [ $RESULT == 0 ]; then
+if [[ $RESULT == 0 ]]; then
     # No errors
-    MESSAGE="Downloaded file ${FILENAMETITLE} for podcast ${PODCASTTITLE} successfully!"
+    TITLE="Success!"
+    MESSAGE="Downloaded file ${FILENAMETITLE} for podcast ${PODCASTTITLE}!"
 else
     # Some error happened!
+    TITLE="ERROR!"
     MESSAGE="Error code ${RESULT} encountered while downloading file ${FILENAMETITLE} for podcast ${PODCASTTITLE}! See man wget for more details."
 fi
 
-apprise -t "GREG" -b "$MESSAGE" "$APPRISE_SERVICE"
+apprise -t "$TITLE" -b "$MESSAGE" "$APPRISE_SERVICE"
